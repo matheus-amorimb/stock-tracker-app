1a5186d62c02bd6808352a94e9adfbd44f62e79a
Matheus Batista <mbatista.sarti@gmail.com>
2024-12-15 21:25:46 -0300

    feat(db): register entity modifications while saving

---
+++ Services/Stocks/Stocks.API/Data/AuditableEntityInterceptor.cs
@@ -0,0 +1,28 @@
+namespace Stocks.API.Data;
+
+public class AuditableEntityInterceptor : SaveChangesInterceptor
+{
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result,
+        CancellationToken cancellationToken = new CancellationToken())
+    {
+        UpdateEntities(eventData.Context);
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private void UpdateEntities(DbContext? context)
+    {
+        if (context == null) return;
+        foreach (var entry in context.ChangeTracker.Entries<IEntity>())
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreatedAt = DateTime.UtcNow;
+            }
+
+            if (entry.State is EntityState.Added or EntityState.Modified)
+            {
+                entry.Entity.CreatedAt = DateTime.UtcNow;
+            }
+        }
+    }
+}
