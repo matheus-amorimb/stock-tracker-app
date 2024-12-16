6efc51c81fd95ca2d0c040e80ab605f17bdbca33
Matheus Batista <mbatista.sarti@gmail.com>
2024-12-16 17:59:03 -0300

    feat(*): create jobs for tracking stock alert subscriptions

---
+++ Services/StocksMonitor/StocksMonitorService/Stocks/Workers/StocksMonitor.cs
@@ -0,0 +1,67 @@
+namespace StocksMonitorService.Stocks.Workers;
+
+public class StocksMonitor(StocksService stocksService, CacheRepository cacheRepository, IServiceProvider serviceProvider) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var activeStocks = await cacheRepository.GetActiveStocks();
+            if (!activeStocks.Any()) return;
+            var monitoringTasks = activeStocks.Select(MonitorStocksAsync);
+            await Task.WhenAll(monitoringTasks);
+            await Task.Delay(20000, stoppingToken);
+        }
+    }
+
+    private async Task MonitorStocksAsync(string stockName)
+    {
+        var stockData = await stocksService.GetMostRecentStockDataAsync(stockName);
+        if (stockName != "IBM") return;
+        if (stockData == null) return;
+        
+        var stockDate = DateTime.Parse(stockData.Value.Key);
+        // if (!HasStockDataBeenUpdated(stockDate)) return;
+        if (HasStockDataBeenUpdated(stockDate)) return;
+        
+        Decimal.TryParse(stockData.Value.Value.High, out var stockHighPrice);
+        Decimal.TryParse(stockData.Value.Value.Low, out var stockLowPrice);
+        
+        var subscribers = await cacheRepository.GetSubscriptionsForStock(stockName);
+        foreach (var subscriber in subscribers)       
+        {
+            var buyingPrice = subscriber?.BuyingPrice;
+            var sellingPrice = subscriber?.SellingPrice;
+            
+            if (stockHighPrice >= sellingPrice || stockLowPrice <= buyingPrice)
+            {
+                var priceAlertTriggeredEvent = new PriceAlertTriggeredEvent
+                {
+                    SubscriberName = subscriber?.SubscriberName,
+                    SubscriberEmail = subscriber?.SubscriberEmail,
+                    StockName = stockName,
+                    SellingPrice = subscriber?.SellingPrice,
+                    BuyingPrice = subscriber?.BuyingPrice,
+                    StockHighPrice = stockHighPrice,
+                    StockLowPrice = stockLowPrice,
+                    StockDateTime = stockDate
+                };
+                
+                await PublishEvent(priceAlertTriggeredEvent);
+            }
+        }
+    }
+    
+    private bool HasStockDataBeenUpdated(DateTime lastUpdatedDate)
+    {
+        var dateLimit = lastUpdatedDate.AddMinutes(60);
+        return dateLimit >= DateTime.Now;
+    }
+
+    private async Task PublishEvent(PriceAlertTriggeredEvent priceAlertTriggeredEvent)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+        await mediator.Publish(priceAlertTriggeredEvent);
+    }
+}
