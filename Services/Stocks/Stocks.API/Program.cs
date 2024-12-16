b649601bcd32ebc58bc7a1d73cfcc708c62aa593
Matheus Batista <mbatista.sarti@gmail.com>
2024-12-15 21:41:37 -0300

    feat(*): register services to main program

--- Services/Stocks/Stocks.API/Program.cs
+++ Services/Stocks/Stocks.API/Program.cs
@@ -1,25 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddServices(configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
+app.MapCarter();
 app.MapControllers();
+app.UseExceptionHandler(options => { });
 
 app.Run();
