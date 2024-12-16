e6078a394260d3f83df2810aa8b32f978ec2ca63
Matheus Batista <mbatista.sarti@gmail.com>
2024-12-15 21:35:10 -0300

    feat(broker): set config for message broker

---
+++ Services/Stocks/Stocks.API/MessageBroker/BrokerExtension.cs
@@ -0,0 +1,47 @@
+namespace Stocks.API.MessageBroker;
+
+public static class BrokerExtension
+{
+    public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMassTransit(cfg =>
+        {
+            cfg.SetKebabCaseEndpointNameFormatter();
+
+            cfg.AddConsumer<ConsumerExample>();
+            
+            cfg.UsingRabbitMq(((context, configurator) =>
+            {
+                var hostUri = new Uri(configuration.GetValue<string>("MessageBroker:Host") ?? throw new NullReferenceException());
+                var userName = configuration.GetValue<string>("MessageBroker:UserName") ?? throw new NullReferenceException();
+                var password = configuration.GetValue<string>("MessageBroker:Password") ?? throw new NullReferenceException();
+                var subscribedTopicName = configuration.GetValue<string>("MessageBroker:SubscribedTopic") ?? throw new NullReferenceException();
+                var unsubscribedTopicName = configuration.GetValue<string>("MessageBroker:UnsubscribedTopic") ?? throw new NullReferenceException();
+                var subscribedEventQueueName = configuration.GetValue<string>("MessageBroker:SubscribedEventQueue") ?? throw new NullReferenceException();
+                
+                configurator.Host(hostUri, hostConfigurator =>
+                {
+                    hostConfigurator.Username(userName);
+                    hostConfigurator.Password(password);
+                });
+                configurator.Message<SubscribedEvent>(topologyConfigurator =>
+                {
+                    topologyConfigurator.SetEntityName(subscribedTopicName);
+                });
+                
+                configurator.Message<UnsubscribedEvent>(topologyConfigurator =>
+                {
+                    topologyConfigurator.SetEntityName(unsubscribedTopicName);
+                });
+                
+                configurator.ReceiveEndpoint(subscribedEventQueueName, endpointConfigurator =>
+                {
+                    endpointConfigurator.ConfigureConsumeTopology = false;
+                    endpointConfigurator.ConfigureConsumer<ConsumerExample>(context);
+                    endpointConfigurator.Bind(subscribedTopicName);
+                });
+            }));
+        });
+        return services;
+    }
+}
