35ecc3eb55f49aca9283564b1e3c66d2ebfca43e
Matheus Batista <mbatista.sarti@gmail.com>
2024-12-17 09:02:29 -0300

    feat(StocksMonitor): config quartz

--- Services/StocksMonitor/StocksMonitorService/DependencyInjection.cs
+++ Services/StocksMonitor/StocksMonitorService/DependencyInjection.cs
@@ -13,6 +13,24 @@ public static class DependencyInjection
         services.AddSingleton<StocksService>();
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
         services.AddMessageBroker(configuration);
+        services.AddScheduler(configuration);
+        return services;
+    }
+
+    private static IServiceCollection AddScheduler(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddQuartz(q =>
+        {
+            var jobKey = new JobKey("StocksMonitorJob");
+            q.AddJob<StocksMonitor>(opts => opts.WithIdentity(jobKey));
+            q.AddTrigger(opts => opts
+                .ForJob(jobKey)
+                .WithIdentity("StocksMonitorTrigger")
+                .WithSimpleSchedule(x => x
+                    .WithInterval(TimeSpan.FromMinutes(60))
+                    .RepeatForever()));
+        });
+        services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
         return services;
     }
 }
